1120c1b99715b5d7fcac229b2e13583b3432820e
Michal Marek <michal@markovi.net>
2021-12-13 21:45:07 +0100

    Day 10

---
+++ src/com/company/Day10.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.function.Predicate;
+
+public class Day10 extends Puzzle {
+    public Day10(List<String> lines) {
+        super(lines);
+    }
+
+    private static final Map<Character, Integer> costUnexpected = Map.of(
+            ')', 3,
+            ']', 57,
+            '}', 1197,
+            '>', 25137
+    );
+    private static final Map<Character, Integer> costMissing = Map.of(
+            ')', 1,
+            ']', 2,
+            '}', 3,
+            '>', 4
+    );
+
+    private long parseLine(String line, boolean evaluateUnexpected) {
+        Stack<Character> expect = new Stack<>();
+
+        for (var c : line.toCharArray()) {
+            switch (c) {
+                case '(':
+                    expect.push(')');
+                    break;
+                case '[':
+                    expect.push(']');
+                    break;
+                case '{':
+                    expect.push('}');
+                    break;
+                case '<':
+                    expect.push('>');
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                case '>':
+                    if (c != expect.pop()) {
+                        if (evaluateUnexpected) {
+                            return costUnexpected.get(c);
+                        } else {
+                            return 0;
+                        }
+                    }
+            }
+        }
+        if (evaluateUnexpected) {
+            return 0;
+        }
+        long score = 0;
+        while (!expect.empty()) {
+            score *= 5;
+            score += costMissing.get(expect.pop());
+        }
+        return score;
+    }
+
+    @Override
+    public long runIt() {
+        //return lines.stream().mapToLong(l -> parseLine(l, true)).sum();
+        var scores = lines.stream().mapToLong(l -> parseLine(l, false)).filter(i -> i != 0).sorted().toArray();
+        return scores[scores.length / 2];
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,7 +19,7 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Puzzle p = new Day9(input);
+        Puzzle p = new Day10(input);
         System.out.println(p.runIt());
     }
 }
